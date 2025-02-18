bf53bf0711991bb80c6f0619223dbbe0dcfa06f1
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-02-18 11:46:14 +0000

    setup hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    calibration: {
+      url: process.env.CALIBRATION_TESTNET_RPC_URL,
+      accounts: [`${process.env.CALIBRATION_TESTNET_PRIVATE_KEY}`]
+    }
+  }
+};
+
+export default config;
