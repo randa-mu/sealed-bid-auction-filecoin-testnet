b14c3991c96356f2ff336a109112e95bae2c73ef
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-02-19 15:29:49 +0000

    update deployment script

--- ignition/modules/SealedBidAuction.ts
+++ ignition/modules/SealedBidAuction.ts
@@ -6,7 +6,6 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const SealedBidAuctionModule = buildModule("SealedBidAuctionModule", (m) => {
   const biddingEndBlock = 45n;
   const blocklockContractAddr = "0xfF66908E1d7d23ff62791505b2eC120128918F44"; 
-
   const sealedBidAuction = m.contract("SealedBidAuction", [biddingEndBlock, blocklockContractAddr], {});
 
   return { sealedBidAuction };
