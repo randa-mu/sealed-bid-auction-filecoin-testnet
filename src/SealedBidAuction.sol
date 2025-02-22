cebb7068173a7b662aa73585b85928f76d90d987
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-02-22 02:06:41 +0000

    update compiler version

--- src/SealedBidAuction.sol
+++ src/SealedBidAuction.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.28;
 
 // Import the Types library for managing ciphertexts
 import {TypesLib} from "@blocklock-solidity/src/libraries/TypesLib.sol";
